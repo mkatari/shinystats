f876bfdc7e22e6af35e0641234956884b0f18daa
Manpreet S. Katari <msk8@nyu.edu>
2020-06-27 23:15:40 -0400

    initial commit

---
+++ ShinyStats/app.R
@@ -0,0 +1,59 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Number of samples"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("n",
+                        "Number of bins:",
+                        min = 1,
+                        max = 10,
+                        value = 5)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        D_placebo=numeric()
+        D_drug= numeric()
+        
+        for (i in 1:10000) {
+            D_placebo[i] = mean(Placebo[sample(1:length(Placebo), 
+                                               input$n, replace=T)])
+            D_drug[i] = mean(Drug[sample(1:length(Drug), 
+                                         input$n, 
+                                         replace=T)])
+        }
+        datamelt = melt(cbind(D_placebo,D_drug), 
+                        varnames = c("id","treatment"))
+        
+        ggplot(datamelt) + geom_histogram(mapping=aes(x=value, 
+                                                      fill=treatment))
+        
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
